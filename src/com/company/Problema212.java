7bcdaa53f5eea2f801f884083a0fe938cfecdf33
asixdam1b <LuisT2000>
2018-10-30 16:57:08 +0100

    Luis

---
+++ src/com/company/Problema212.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Problema212 {
+
+    public static void main(String[] args) {
+        int j=45;
+
+    }
+}
---
+++ src/com/company/Problema67.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Problema67 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
